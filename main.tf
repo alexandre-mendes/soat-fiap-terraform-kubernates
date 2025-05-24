4520010262de76e1c85d7602a9468bc349d5e580
Alexandre Mendes <59848642+alexandre-mendes@users.noreply.github.com>
2025-05-24 17:44:18 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  version = "5.1.0"
 
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
@@ -17,32 +17,25 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  map_public_ip_on_launch  = true
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 }
 
-resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = var.lab_role_arn
-  version  = "1.32"
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4"
 
-  vpc_config {
-    subnet_ids = module.vpc.public_subnets
-  }
-}
-
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "academy-nodes"
-  node_role_arn   = var.lab_role_arn
+  cluster_name    = "meu-cluster"
+  cluster_version = "1.29"
   subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+    }
   }
-
-  instance_types = ["t3.medium"]
 }
